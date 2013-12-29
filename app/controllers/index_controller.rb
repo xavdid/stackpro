ca81bb8044d74859eae8aeeaa1e780a1c59acefa
David Brownman <beamneocube@gmail.com>
2013-12-29 14:18:13 -0600

    changed some comments

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,21 +4,21 @@ class IndexController < ApplicationController
   def home
     read_db
     t = Time.now
-    #this should get the past 24 hours worth of points, maybe displaying 4 at a time by default?
+    
     prev = 48
 
-    # most recent PREV data points 
+     
     # shouldn't need to sort, data will always be in order it went int
     # just in case: .sort({'unix'=>1})
     @response = @coll.find({:unix=>{'$gt'=>t.to_i-(3600*prev)}}).to_a
 
-    # make this callable, or just always pass front the max and slice what js shows?
+    
     @dates = []
     @asked = []
     @unanswered = []
     @percentage = []
 
-    # I actually should, the more intense processing should be server side
+   
     @response.each do |i|
       @dates.push(i["timestamp"])
       @asked.append(i["asked"])
@@ -29,6 +29,7 @@ class IndexController < ApplicationController
 
   end
 
+  # surely there's a better way to do this
   def read_db
     @client ||= Mongo::MongoClient.from_uri('mongodb://d:b@ds053428.mongolab.com:53428/kinect')
     @db ||= @client.db('kinect')
